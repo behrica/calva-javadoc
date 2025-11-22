0b9fcd4430863624031a27cbb7a257dd29da24aa
Carsten Behring <carsten.behring@gmail.com>
2025-11-22 22:00:54 +0100

    added flare tes

--- test/calva_javadoc_test.clj
+++ test/calva_javadoc_test.clj
@@ -1,5 +1,5 @@
 (ns test.calva-javadoc-test
-  
+
   (:require [calva-javadoc :refer [to-class-instance-form view-javadoc-in-flare]]))
 
 
@@ -8,11 +8,12 @@
 (assert (= "java.util.UUID/randomUUID" (to-class-instance-form '(java.util.UUID/randomUUID))))
 (def uuid (java.util.UUID/randomUUID))
 (assert (= "java.util.UUID/.toString" (to-class-instance-form '(.toString uuid))))
+(assert (= "java.util.UUID/.toString" (to-class-instance-form '(.toString ^UUID uuid))))
 (assert (= "java.lang.String/.charAt" (to-class-instance-form '(String/.charAt))))
 (assert (= "java.util.UUID/.toString" (to-class-instance-form '(.toString (java.util.UUID/randomUUID)))))
+(assert (= "java.util.UUID/.getLeastSignificantBits" (to-class-instance-form '(.getLeastSignificantBits uuid))))
+(assert (= "sun.util.calendar.ZoneInfo/.setRawOffset" (to-class-instance-form '(.setRawOffset tz))))
 
-(view-javadoc-in-flare '(java.util.UUID/randomUUID))
 
-
-
-(java.util.UUID/randomUUID)
+(def flare (calva-javadoc/view-javadoc-in-flare '(.getLeastSignificantBits uuid)))
+(assert (= 'flare/html (:tag flare)))
